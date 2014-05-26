a4a1b0bfab566365ff5af3f001058a05a16def38
BeterTugeder <qba307@gmail.com>
2014-05-26 21:58:02 +0200

    Merge origin/master
    Conflicts:
    	src/main/java/pl/projekt5/dziennik/App.java

--- src/main/java/pl/projekt5/dziennik/App.java
+++ src/main/java/pl/projekt5/dziennik/App.java
@@ -52,10 +52,6 @@ public class App
         //testowy komentarz
         //commit
         
-        
-
-        //tutaj jakis test...
-        
         LogHandler.close();
         
         ModelFactory.closeConnection();
